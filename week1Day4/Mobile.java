8496e9e4d433f60e6ef6202114e4aff8f33c8f0c
VishnuprathapM <142899861+VishnuprathapM@users.noreply.github.com>
2024-08-22 21:31:35 +0530

    Add files via upload
    Assignment 1

---
+++ week1Day4/Mobile.java
@@ -0,0 +1,33 @@
+package week1Day4;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		String mobileName="Samsung";
+		String mobileModel="S24";
+		int mobilePrice=30000;
+		float mobileOffer=5.12345f;
+		double mobileSARValue=456798.8657889990;
+		char mobileLogo='S';
+		long imeNumber=67890646736567484l;
+		boolean isDisplayed=true;
+		
+		
+		System.out.println(mobileName);
+		System.out.println(mobileModel);
+		System.out.println(mobilePrice);
+		System.out.println(mobileOffer);
+		System.out.println(mobileSARValue);
+		System.out.println(mobileLogo);
+		System.out.println(imeNumber);
+		System.out.println(isDisplayed);
+		
+		
+		System.out.println(mobileName+" "+mobileModel+" "+mobilePrice+" "+mobileOffer+" "+mobileSARValue+" "+mobileLogo+" "+imeNumber+" "+isDisplayed);
+      
+		System.out.println(mobileName+"\n"+mobileModel+"\nn"+mobilePrice+"\n"+mobileOffer+"\n"+mobileSARValue+"\n"+mobileLogo+"\n"+imeNumber+"\n"+isDisplayed);
+
+		
+	}
+
+}
